d4231d1d9cb2cd2a91ce5554f503e8098c9fdebc
Damianos sotirakis <damianos.oliveira@pdcase.com.br>
2018-11-24 22:55:18 -0200

    Início do curso (ambientação)
    Render page / thymeleaf
    TH, Bootstrap, List.

--- src/main/java/com/example/WebApp/controller/HelloWordController.java
+++ src/main/java/com/example/WebApp/controller/HelloWordController.java
@@ -1,5 +1,8 @@
 package com.example.WebApp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloWordController {
 	
 	@GetMapping
-	public ModelAndView index(Model model) {
+	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("/hello");
+		mv.addObject("name", "Teste renderização");
 		return mv;
+	}
+	
+	@GetMapping("/new")
+	public String newPath(Model model) {
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Leonan");
+		list.add("Wesley");
+		list.add("Victor");
+		list.add("Luiz");
+		list.add("Erik");
+		
+		model.addAttribute("name", "Leonan");
+		model.addAttribute("list", list);
+		
+		return "new";
 	}
 
 }
